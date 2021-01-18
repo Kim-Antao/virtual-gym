b8f50a17b92edb33ae0884e160c40ba1298126d5
Kim-Antao <kimberley2609@gmail.com>
2021-01-18 00:31:08 +0000

    custom storage file updated

--- virtual_gym/settings.py
+++ virtual_gym/settings.py
@@ -111,8 +111,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -183,6 +181,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'virtual-gym-1'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -217,4 +221,15 @@ STRIPE_S_PLAN_ANNUAL_ID = os.getenv('STRIPE_S_PLAN_ANNUAL_ID', '')
 STRIPE_G_PLAN_MONTHLY_ID = os.getenv('STRIPE_S_PLAN_MONTHLY_ID', '')
 STRIPE_G_PLAN_QUATERLY_ID = os.getenv('STRIPE_S_PLAN_QUATERLY_ID', '')
 STRIPE_G_PLAN_ANNUAL_ID = os.getenv('STRIPE_S_PLAN_ANNUAL_ID', '')
-DEFAULT_FROM_EMAIL = 'virtualgym@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'virtualgym@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
